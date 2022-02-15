98bf96da61d54e748cf3035610c8bc6fddade0f9
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2022-02-15 23:08:44 +0800

    完成不可在有效期限內，建立相同課程訂單

--- app/api/api_v0/orders.rb
+++ app/api/api_v0/orders.rb
@@ -17,16 +17,21 @@ module ApiV0
       requires :amount, type: Integer
     end
     post "/build/order" do
-      order = current_user.orders.new(declared(params, include_missing: false).except(:access_key))
-
-      if order.save
-        valid_time = Course.find_by(id:params[:course_id]).valid_time
-        VaildDaysJob.set(wait: valid_time.days).perform_later(order)
-        present order, with: ApiV0::Entities::Order
-        
+      all_courses = current_user.orders.pluck(:course_id)
+      if all_courses.include?(params[:course_id]) && current_user.orders.find_by(course_id:params[:course_id]).state == "vaild"
       else
-        raise StandardError, $!
+        order = current_user.orders.new(declared(params, include_missing: false).except(:access_key))
+        if order.save
+          valid_time = Course.find_by(id:params[:course_id]).valid_time
+          VaildDaysJob.set(wait: valid_time.days).perform_later(order)
+          present order, with: ApiV0::Entities::Order      
+        else
+          raise StandardError, $!
+        end
       end
+    
+
+    
     end
   end
 end
