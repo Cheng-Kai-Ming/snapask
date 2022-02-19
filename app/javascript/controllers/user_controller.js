fc247bdc1676a24e188bc9c29ea768f7de072b7f
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2022-02-19 23:37:42 +0800

    可以渲染API資料

--- app/javascript/controllers/user_controller.js
+++ app/javascript/controllers/user_controller.js
@@ -3,8 +3,10 @@ import { Controller } from "stimulus"
 
 
 export default class extends Controller {
-  courses(){
+  static targets = ["output"]
 
+  courses(){
+      let coursesHTLM = ""
       fetch('/api/v0/courses',{
         headers:{
           "X-Api-Secret-Key":"secret"
@@ -12,19 +14,49 @@ export default class extends Controller {
         params:{access_key: "Jazyx5znADzoubQ53zZIg0FxxvBmDcg3MVgP2dEY"}
       })
         .then(function(response) {return response.json()} )
-        .then(function(json){console.log(json)}) 
-    
+        .then((courses)=>{
+          courses.forEach(function(element){
+            coursesHTLM += 
+          `
+          <div>
+            <tr>
+              <td>${element.name}</td>
+              <td>${element.price}</td>
+              <td>${element.currency}</td>
+              <td>${element.category}</td>
+              <td>${element.intro}</td>
+            </tr> 
+          </div>
+          `
+        })
+        this.outputTarget.innerHTML += coursesHTLM
+        }) 
   }
 
   orders(){
-
+    let ordersHTLM = ""
     fetch('/api/v0/orders?access_key=qBvsmmlntfHhbKre570h7es9BJjUM6qDT6gVzj0E',{
       headers:{
         "X-Api-Secret-Key":"secret",
       }
     })
       .then(function(response) {return response.json()} )
-      .then(function(json){console.log(json)}) 
+      .then((orders)=>{
+        orders.forEach(function(element){
+          ordersHTLM +=
+          `
+          <div>
+            <tr>
+              <td>${element.amount}</td>
+              <td>${element.paid_at}</td>
+              <td>${element.serial}</td>
+              <td>${element.state}</td>
+            </tr> 
+          </div>
+        `
+        })
+        this.outputTarget.innerHTML += ordersHTLM
+      }) 
   
   }
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => 'admin/sidekiq'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/view/courses', to: "courses#user"
+  root to: "courses#user"
 end
